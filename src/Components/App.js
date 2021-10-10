9e300201a9764a28836a5ab64236f51c33e9945b
durgesh gupta <durgesh10gupta@gmail.com>
2021-10-10 12:33:07 +0530

    Clean the provious code of router

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,28 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 
-const PageOne = () => {
-  return (
-    <div>
-      <Link to="/pagetwo">Navigate to Page 2</Link>
-    </div>
-  );
-};
-const PageTwo = () => {
-  return (
-    <div>
-      <Link to="/">Navigate to Page 1</Link>
-    </div>
-  );
-};
-
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
-            <Route path="/" exact component={PageOne}/>
-            <Route path="/pagetwo" exact component={PageTwo}/>
+           
         </div>
       </BrowserRouter>
     </div>
