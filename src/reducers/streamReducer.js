69e81aa0d12cce95b85860b21625dafc76f133dc
durgesh gupta <durgesh10gupta@gmail.com>
2021-10-12 14:01:36 +0530

    HANDLE fetch,create,and edit

---
+++ src/reducers/streamReducer.js
@@ -0,0 +1,27 @@
+import actions from "redux-form/lib/actions"
+import {FETCH_STREAM,
+FETCH_STREAMS,
+DELETE_STREAM,
+EDIT_STREAM,
+CREATE_STREAM
+} from "../actions/types"
+
+
+export default (state ={},action)=>{
+    switch(action.type){
+        case FETCH_STREAM:
+            return {...state,[action.payload.id]:action.payload}
+
+        case CREATE_STREAM:
+            return {...state,[action.payload.id]:action.payload}
+
+        case EDIT_STREAM:
+            return {...state,[action.payload.id]:action.payload}
+
+
+
+
+        default:
+            return state;
+    }
+}
